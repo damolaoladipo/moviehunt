1cb7cd24ab13c6d42a13465a2eee0806e1ce7e02
Damola <odamola@mit.edu>
2024-10-08 00:28:17 +0100

    implemented movie card UI

--- js/moviecard.js
+++ js/moviecard.js
@@ -5,7 +5,7 @@ const movieData = [
       ratings: '8.2/10',
       genre: 'Sci-Fi, Action',
       duration: '1h 40m',
-      poster: 'https://via.placeholder.com/300x450'
+      poster: '../assets/abyss.jpg'
   },
   {
       title: 'Bionic',
@@ -13,7 +13,7 @@ const movieData = [
       ratings: '8.9/10',
       genre: 'Sci-Fi, Thriller',
       duration: '2h 25m',
-      poster: 'https://via.placeholder.com/300x450'
+      poster: '../assets/bionic.jpg'
   },
   {
       title: 'Code 8: Part II',
@@ -21,7 +21,7 @@ const movieData = [
       ratings: '6.2/10',
       genre: 'Sci-Fi, Action, Crime',
       duration: '1h 45m',
-      poster: 'https://via.placeholder.com/300x450'
+      poster: '../assets/code8.jpg'
   },
   {
       title: 'Rebel Moon',
@@ -29,7 +29,7 @@ const movieData = [
       ratings: '7.3/10',
       genre: 'Sci-Fi, Fantasy, Action',
       duration: '2h 10m',
-      poster: 'https://via.placeholder.com/300x450'
+      poster: '../assets/rebel-moon.jpg'
   },
   {
       title: 'Uglies',
@@ -37,7 +37,7 @@ const movieData = [
       ratings: '6.6/10',
       genre: 'Sci-Fi, Adventure',
       duration: '1h 55m',
-      poster: 'https://via.placeholder.com/300x450'
+      poster: '../assets/uglies.jpg'
   },
   {
       title: 'Rebel Ridge',
@@ -45,7 +45,55 @@ const movieData = [
       ratings: '7.9/10',
       genre: 'Action, Thriller',
       duration: '1h 50m',
-      poster: 'https://via.placeholder.com/300x450'
+      poster: '../assets/rebel-ridge.jpg'
+  },
+  {
+      title: 'The Adam Project',
+      year: '2022',
+      ratings: '6.7/10',
+      genre: 'Sci-Fi, Adventure, Comedy',
+      duration: '1h 46m',
+      poster: '../assets/theadamproject.jpg'
+  },
+  {
+      title: 'Project Power',
+      year: '2020',
+      ratings: '6.0/10',
+      genre: 'Sci-Fi, Action, Thriller',
+      duration: '1h 53m',
+      poster: '../assets/projectpower.jpg'
+  },
+  {
+      title: 'The Midnight Sky',
+      year: '2020',
+      ratings: '5.6/10',
+      genre: 'Sci-Fi, Drama',
+      duration: '1h 58m',
+      poster: '../assets/themidnightsky.jpg'
+  },
+  {
+      title: 'I Am Mother',
+      year: '2019',
+      ratings: '6.7/10',
+      genre: 'Sci-Fi, Thriller',
+      duration: '1h 53m',
+      poster: '../assets/iammother.jpg'
+  },
+  {
+      title: 'Black Mirror: Bandersnatch',
+      year: '2018',
+      ratings: '7.1/10',
+      genre: 'Sci-Fi, Drama',
+      duration: '1h 30m',
+      poster: '../assets/bandersnatch.jpg'
+  },
+  {
+      title: 'Another Life',
+      year: '2019',
+      ratings: '5.2/10',
+      genre: 'Sci-Fi, Drama, Mystery',
+      duration: '1h 42m',
+      poster: '../assets/anotherlife.jpg'
   }
 ];
 
@@ -74,3 +122,4 @@ function renderMovies(movies) {
 }
 
 renderMovies(movieData);
+gitt
